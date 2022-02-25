a9ac6280f4cec6ae3527069aef555d0a2fda585a
rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>
2022-02-25 13:32:20 +0100

    made an array with objects

---
+++ database.js
@@ -0,0 +1,100 @@
+"use strict";
+
+// my database of countries
+
+let database = [
+
+    {
+        Id: 1,
+        Country: "Spain",
+        Capital: "Madrid",
+        Inhabitant : 43,
+        Euro: true
+
+    },
+
+    {
+        Id: 2,
+        Country: "Indonesia",
+        Capital: "Jakarta",
+        Inhabitant : 274,
+        Euro : false
+
+    },
+
+    {
+        Id: 3,
+        Country: "Finland",
+        Capital: "Helsinki",
+        Inhabitant : 5,
+        Euro: true
+
+    },
+
+    {
+        Id: 4,
+        Country: "Marocko",
+        Capital: "Rabat",
+        Inhabitant : 37,
+        Euro : false
+
+    },
+
+    {
+        Id: 5,
+        Country: "Argentina",
+        Capital: "Buenos Aries",
+        Inhabitant : 45,
+        Euro: false
+
+    },
+
+
+    {
+        Id: 6,
+        Country: "Australia",
+        Capital: "Canberra",
+        Inhabitant : 26,
+        Euro: false
+
+    },
+
+
+    {
+        Id: 7,
+        Country: "Canada",
+        Capital: "Ottawa",
+        Inhabitant : 38,
+        Euro: false
+
+    },
+
+    {
+        Id: 8,
+        Country: "Poland",
+        Capital: "Warsaw",
+        Inhabitant : 38,
+        Euro : false
+
+    },
+
+    {
+        Id: 9,
+        Country: "Madagascar",
+        Capital: "Antananarivo",
+        Inhabitant : 28,
+        Euro: false
+    },
+
+    {
+        Id: 10,
+        Country: "Japan",
+        Capital: "Tokyo",
+        Inhabitant : 126,
+        Euro: false
+
+    },
+
+
+
+]
