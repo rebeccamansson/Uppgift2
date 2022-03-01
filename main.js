c342b4f8a49c93828d2c13253b4dd893b17e48b8
rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>
2022-03-01 19:52:38 +0100

    done with code so the remove buttons work

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function getCountry(country) {
     div.id = country.id;
 
     div.innerHTML = `
+
     <div> ${country.country}</div>
     <div> ${country.capital}</div>
     <div> ${country.inhabitant}</div>
@@ -31,7 +32,7 @@ function getCountries(countries) {
 }
 
 
-// Made a function to make a new country
+// a function to make a new country
 function newCountry(country, capital, inhabitant, language) {
     let Country = {
         country: country,
@@ -44,10 +45,46 @@ function newCountry(country, capital, inhabitant, language) {
 
 }
 
-// Made a function to add the new country to the database
-function addNewCountryToDatabase (database,country){
-database.push(country);
+// a function to add the new country to the database
+function addNewCountryToDatabase(database, country) {
+    database.push(country);
+}
+
+// a function that removes a country from the database based on its name
+
+function removeCountry(countries, id) {
+    for (let i = 0; i < countries.length; i++) {
+        let country = countries[i];
+        if (country.id == id) {
+            countries.splice(i, 1);
+            return;
+        }
+
+    }
+
+}
+// to know when the user clicks sppecific on the remove button
+
+function onClickRemoveCountry(event) {
+    let button = event.target;
+    let id = button.parentElement.id;
+    removeCountry(database, id);
+    getCountries(database);
+    handlerOnRemoveButton();
+
+}
+
+// a function that adds a click eventlistener to all the removebuttons
+
+function handlerOnRemoveButton() {
+    let buttons = document.querySelectorAll(".country button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", onClickRemoveCountry);
+    }
 }
 
 
- getCountries(database);
+getCountries(database);
+handlerOnRemoveButton();
+
