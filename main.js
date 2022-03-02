8b551b06570d1c8b61f4064f53bf635ad2f53c08
rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>
2022-03-02 17:33:28 +0100

    made functions so you can filter by country and capital

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ function getCountries(countries) {
     }
 }
 
-
 // a function to make a new country
 function newCountry(country, capital, inhabitant, language) {
     let Country = {
@@ -50,7 +49,7 @@ function addNewCountryToDatabase(database, country) {
     database.push(country);
 }
 
-// a function that removes a country from the database based on its name
+// a function that removes a country from the database based on its id
 
 function removeCountry(countries, id) {
     for (let i = 0; i < countries.length; i++) {
@@ -63,7 +62,7 @@ function removeCountry(countries, id) {
     }
 
 }
-// to know when the user clicks sppecific on the remove button
+// to know when the user clicks specific on the remove button
 
 function onClickRemoveCountry(event) {
     let button = event.target;
@@ -84,7 +83,103 @@ function handlerOnRemoveButton() {
     }
 }
 
+function writeInCountry(event) {
 
+    event.preventDefault();
+
+    let country = document.getElementById("country").value;
+    let capital = document.getElementById("capital").value;
+    let inhabitant = Number(document.getElementById("inhabitant").value);
+    let language = document.getElementById("language").value;
+
+    if (country == "") {
+        return alert("Fill in the country please.")
+    }
+
+    else if (capital == "") {
+        return alert("Fill in the capital please.")
+    }
+
+    else if (inhabitant == 0) {
+        return alert("Fill in the inhabitant please.")
+    }
+
+    else if (language == "") {
+        return alert("Fill in the language please.")
+    }
+
+
+    let Country = newCountry(country, capital, inhabitant, language);
+    Country.id = database[database.length - 1].id + 1;
+
+    addNewCountryToDatabase(database, Country);
+    getCountries(database);
+    
+
+    let form = document.getElementById("fillincountry");
+    form.reset();
+
+}
+
+function handlerOnAddButton() {
+    let form = document.getElementById("fillincountry");
+    form.addEventListener("submit", writeInCountry);
+}
+
+function getCountrybyCountry(countries, country) {
+    let countryByCountry = [];
+    for (let Country of countries) {
+        if (Country.country.toLowerCase()== country.toLowerCase()) {
+            countryByCountry.push(Country);
+        }
+    }
+    return countryByCountry;
+}
+
+function getCountrybyCapital(countries, capital) {
+    let countryByCapital = [];
+    for (let Country of countries) {
+        if (Country.capitaltoLowerCase() == capital.toLowerCase()) {
+            countryByCapital.push(Country);
+        }
+    }
+    return countryByCapital;
+}
+
+function filterCountryByCountry (event){
+    event.preventDefault ();
+    let country = document.getElementById ("filtercountry").value;
+    let countries = getCountrybyCountry (database, country)
+    getCountries (countries);
+}
+
+function filterCountryByCapital (event){
+    event.preventDefault ();
+    let capital = document.getElementById ("filtercapital").value;
+    let countries = getCountrybyCapital (database, capital)
+    getCountries (countries);
+}
+
+function showAllClick (){
+    document.getElementById ("filtercountry").value ="";
+    document.getElementById ("filtercapital").value ="";
+    getCountries (database);
+    
+}
+
+function handlerOnFilter (){
+    let countryForm = document.getElementById ("filterbycountry");
+    let capitalForm = document.getElementById ("filterbycapital");
+    let showAllButton = document.getElementById ("showallcountries");
+
+    countryForm.addEventListener ("submit", filterCountryByCountry);
+    capitalForm.addEventListener ("submit", filterCountryByCapital);
+    showAllButton.addEventListener ("click", showAllClick);
+}
+
+getCountry(country) ;
 getCountries(database);
+handlerOnAddButton();
+handlerOnFilter();
 handlerOnRemoveButton();
 
